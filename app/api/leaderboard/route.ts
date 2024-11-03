a02850741b2f9e3e60f4d5b77d5d7cd1f42981cf
joconnell34 <39654943+joconnell34@users.noreply.github.com>
2024-11-03 15:23:02 -0800

    force dynamic

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 import axios from "axios";
 import { getTokenData } from "@/lib/helpers";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
